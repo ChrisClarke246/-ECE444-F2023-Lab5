577252a8c76a1499833d2978d27b9cad8cfa4af5
CHRISTIAN MCINTOSH_CLARKE <clarkechristian5@gmail.com>
2023-10-28 16:11:17 -0400

    test login required and implement it

--- tests/app_test.py
+++ tests/app_test.py
@@ -80,9 +80,18 @@ def test_search(client):
     rv = client.get('/search/')
     assert b"entries" in rv.data
 
+def test_login_required(client):
+    """Ensure you must be logged in"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
-
